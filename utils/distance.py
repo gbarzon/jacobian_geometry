bb5344905a7e69e305cc06223788f8f9f66cebda
Giacomo Barzon (37o) <barzon@brain01.pnc.unipd.it>
2022-07-22 13:00:03 +0200

    Initial wrap with R, creating corrplot

--- utils/distance.py
+++ utils/distance.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from scipy.linalg import expm, eig
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from scipy.spatial.distance import squareform
+from sklearn.metrics import silhouette_score
+
+from utils.plotter import plot_clustermap
 
 import igraph as ig
 import networkx as nx
@@ -12,7 +15,12 @@ from tqdm.auto import tqdm
 import utils.CommonFunctions as CF
 
 
-def laplacian(mat, A=1., B=1.):
+def compute_laplacian(mat, args = []):
+    if len(args) > 0:
+        A, B = args[0], args[1]
+    else:
+        A, B = 1., 1.
+    print(A, B)
     return A * np.eye(len(mat)) - B * mat / np.sum(mat, axis=1)[:,None]
 
 def compute_distance(mat_enr, t):
@@ -27,10 +35,6 @@ def compute_distance(mat_enr, t):
             d_ij[i,j] = np.sqrt(d_ij_tmp.dot(d_ij_tmp))
 
     return d_ij + d_ij.T
-
-def diffusion_distance(mat, t):
-    lapl = laplacian(mat)
-    return compute_distance(-lapl, t)
 
 def average_distance(mat_enr, tmax=None, display=True, return_snapshot=False):
     N = mat_enr.shape[0]
@@ -62,7 +66,7 @@ def plot_communities(mat, comms, ax=None):
     nx.draw(nx.from_numpy_array(mat), node_color=node_color, with_labels=False, ax=ax)
     #plt.show()
 
-def clustering_diffusion_distance(mat, precomputed=None, show=True, method='complete'):
+def diffusion_distance(mat, show=True, method='ward', args=[], name=None):
     '''
     method = single, complete, average, weighted, centroid, median, ward
     '''
@@ -70,63 +74,64 @@ def clustering_diffusion_distance(mat, precomputed=None, show=True, method='comp
     print('DIFFUSION DISTANCE')
     N = mat.shape[0]
     
+    # Compute laplacian...
+    print('- Compute laplacian...')
+    laplacian = compute_laplacian(mat, args)
+    
     # Compute average diffusion distance
-    if precomputed is None:
-        print('- Compute average distance...')
-        avg_dd = average_distance(-laplacian(mat))
-    else:
-        print('** Distance already computed')
-        avg_dd = precomputed
+    print('- Compute average distance...')
+    
+    avg_dd = average_distance(-laplacian)
     
     # Compute hierarchical clustering
     print('- Compute hierarchical clustering with method {}...'.format(method))
     Z = linkage(squareform(avg_dd), method=method)
     
+    if name is not None:
+        np.savetxt('results/diffusion_'+name, avg_dd)
+    
     if show:
-        plt.figure(figsize=(10,6))
+        f, axs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [1.2, 1]}, figsize=(8,7))
         
-        ax = plt.subplot(2,2,1)
         #plot_communities(mat, best_part, ax)
-        nx.draw(nx.from_numpy_array(mat), ax=ax)
+        nx.draw(nx.from_numpy_array(mat), ax=axs[0,0])
         
-        plt.subplot(2,2,2)
-        eigvals, eigvecs = eig(-laplacian(mat))
-        plt.plot(eigvals.real, eigvals.imag, 'o')
-        plt.xlabel(r'$Re(\lambda)$')
-        plt.ylabel(r'$Im(\lambda)$')
-        plt.title('')
-        
-        plt.subplot(2,2,3)
+        plt.subplot(axs[0,1])
         im = plt.imshow(avg_dd, cmap='cividis')
         plt.colorbar(im, fraction=0.046, pad=0.04)
         plt.axis('off')
-        plt.title('Average')
+        plt.title('Average diffusion distance')
         
-        plt.subplot(2,2,4)
+        plt.subplot(axs[1,0])
+        eigvals, eigvecs = eig(-laplacian)
+        plt.plot(eigvals.real, eigvals.imag, 'o')
+        plt.xlabel(r'$Re(\lambda)$')
+        plt.ylabel(r'$Im(\lambda)$')
+        plt.title('Eigenvalues')
+        
+        plt.subplot(axs[1,1])
         dendrogram(Z)
+        plt.title(f'Method: {method}')
         
         plt.tight_layout()
         plt.show()
+        
+        #plot_clustermap(avg_dd)
     
-    return n_clusters, mean_dd, best_clust, best_part
+    return avg_dd, Z
 
-def clustering_jacobian_distance(mat, dynamics, clust_max=20, tmax=None, norm=False, norm_ind=False, show=True, method='complete', args=[]):
+def jacobian_distance(mat, dynamics, norm=False, show=True, method='ward', args=[], name=None):
     '''
     method = single, complete, average, weighted, centroid, median, ward
     '''
     
-    print('COMMUNITY DETECTION - JACOBIAN DISTANCE')
+    print('JACOBIAN DISTANCE')
     print('Dynamics: '+dynamics)
     N = mat.shape[0]
     
-    # Define total number of clusters to check
-    n_clusters = np.arange(2,clust_max)
-    n_clusters = np.append(n_clusters, [30, 40, 50, 60, 70])
-    mean_dd = np.zeros(len(n_clusters))
-    
     # Get steady state
     initial_state = np.random.random(N)
-    steady_state = CF.Numerical_Integration(mat, dynamics, initial_state, show=True)
+    steady_state = CF.Numerical_Integration(mat, dynamics, initial_state, show=True, args=args)
     
     # Compute jacobian
     jacobian = CF.Jacobian(mat, dynamics, steady_state[-1], norm=norm, args=args)
@@ -139,66 +144,35 @@ def clustering_jacobian_distance(mat, dynamics, clust_max=20, tmax=None, norm=Fa
     print('- Compute hierarchical clustering with method {}...'.format(method))
     Z = linkage(squareform(avg_dd), method=method)
     
-    print('- Loop over induced graphs...')
-    for i, n_clust in enumerate(n_clusters):
-        # Get communities
-        comms = find_comms(Z, n_clust)
-        
-        # Compute induced graph
-        mat_induced = induced_graph(mat, comms)
-        
-        # Integrate dynamics up to steady state
-        initial_state = np.random.random(mat_induced.shape[0])
-        steady_state = CF.Numerical_Integration(mat_induced, dynamics+'_norm', initial_state, show=False)
-        
-        # Coarse-grain steady state
-        #steady_state_induced = np.array( [np.mean(steady_state[-1][comms==idx]) for idx in range(n_clust)] )
-        
-        # Compute jacobian
-        jacobian_induced = CF.Jacobian(mat_induced, dynamics+'_norm', steady_state[-1], norm=norm_ind, args=args)
-        
-        # Compute diff distance on induced graph
-        avg_dd_induced = average_distance(jacobian_induced, display=False, tmax=tmax)
-        
-        #mean_dd[i] = np.mean(avg_dd_induced[np.triu_indices(mat_induced.shape[0],1)])
-        mean_dd[i] = np.mean(avg_dd_induced)
-        
-        #print('[*] n clust = {} - avg diff dist = {}'.format(n_clust, np.round(mean_dd[i],3)))
-    
-    # Get best partition
-    best_clust = n_clusters[np.argmax(mean_dd)]
-    best_part = fcluster(Z, t=best_clust, criterion='maxclust') -1
+    if name is not None:
+        np.savetxt('results/'+dynamics+'_'+str(args)+'_'+name, avg_dd)
     
     if show:
-        plt.figure(figsize=(10,6))
+        f, axs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [1.2, 1]}, figsize=(8,7))
         
-        ax = plt.subplot(2,3,1)
-        plot_communities(mat, best_part, ax)
+        #plot_communities(mat, best_part, ax)
+        nx.draw(nx.from_numpy_array(mat), ax=axs[0,0])
         
-        plt.subplot(2,3,2)
+        plt.subplot(axs[0,1])
+        im = plt.imshow(avg_dd, cmap='cividis')
+        plt.colorbar(im, fraction=0.046, pad=0.04)
+        plt.axis('off')
+        plt.title('Average diffusion distance')
+        
+        plt.subplot(axs[1,0])
         eigvals, eigvecs = eig(jacobian)
         plt.plot(eigvals.real, eigvals.imag, 'o')
         plt.xlabel(r'$Re(\lambda)$')
         plt.ylabel(r'$Im(\lambda)$')
-        plt.title('')
+        plt.title('Eigenvalues')
         
-        plt.subplot(2,3,3)
-        im = plt.imshow(avg_dd, cmap='cividis')
-        plt.colorbar(im, fraction=0.046, pad=0.04)
-        plt.axis('off')
-        plt.title('Average')
-        
-        plt.subplot(2,3,5)
+        plt.subplot(axs[1,1])
         dendrogram(Z)
-        
-        plt.subplot(2,3,6)
-        plt.plot(n_clusters, mean_dd, 'o-')
-        plt.xlabel('Modules')
-        plt.ylabel('Mean dist')
-        plt.plot(n_clusters[np.argmax(mean_dd)], mean_dd[np.argmax(mean_dd)], 'o', c='red', label=f'best={best_clust}')
-        plt.legend()
+        plt.title(f'Method: {method}')
         
         plt.tight_layout()
         plt.show()
+        
+        #plot_clustermap(avg_dd)
     
-    return n_clusters, mean_dd, best_clust, best_part
+    return avg_dd, Z
